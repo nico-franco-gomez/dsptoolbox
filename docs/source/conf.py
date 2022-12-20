6b79854c3522afd42544a631090997ed57de8e85
nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
2022-12-20 14:28:01 +0100

    docs_started

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,40 +1,27 @@
 # Configuration file for the Sphinx documentation builder.
+import piccolo_theme
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'dsptools'
-copyright = '2022, Franco-Gomez'
-author = 'Franco-Gomez'
+copyright = '2022, Nicolas Franco-Gomez'
+author = 'Nicolas Franco-Gomez'
 
-release = '0.1'
-version = '0.1.0'
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# -- General configuration
-
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+extensions = []
 
 templates_path = ['_templates']
+exclude_patterns = []
 
 
-# -- Options for HTML output
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
-# html_theme = 'press'
-# html_theme = 'piccolo_theme'
-# html_theme_path = ["_themes", ]
-html_theme = 'traditional'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_theme = 'piccolo_theme'
+html_static_path = ['_static']
