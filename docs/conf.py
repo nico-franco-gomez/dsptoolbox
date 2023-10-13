1210006f15b1e253b0e7c00f8cbb8c08cf216840
nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
2023-10-13 17:27:20 +0200

    corrected documentation

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,11 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-from ..dsptoolbox import __version__
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+from dsptoolbox import __version__
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
