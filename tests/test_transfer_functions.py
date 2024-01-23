6ba2c16d3014821a0e41fa830649a32a7b71dea6
nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
2024-01-23 11:44:31 +0100

    Squashed commit of the following:
    commit 7b20d06254842b572db25f1627a8d31e5f7ff504
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Tue Jan 23 11:41:03 2024 +0100
    
        changed harmonics to signals, added offset
    
    commit cf37834a5cc847b94504d43e26284cdc2a9059ea
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Fri Jan 19 00:06:17 2024 +0100
    
        harmonics function
    
    commit 3e9db7711cc2c2a0e4cb1d103438dea923b277eb
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Tue Dec 19 16:34:13 2023 +0100
    
        thd from log chirp started
    
    commit be2718878099bc92000fe58a6c640497166190ab
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Wed Nov 29 16:50:51 2023 +0100
    
        signal class: type safety and doc strings
    
    commit ce10658bb924acf19547e1ec12f14275d9bd242c
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Tue Nov 28 13:32:52 2023 +0100
    
        corrected log fade, updated version number
    
    commit 614b6bb82d37d44bb6aa0840dad080d5e72aa376
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Sun Nov 26 21:19:36 2023 +0100
    
        small, unimportant change
    
    commit 44d614147cdf7ab782be9ce8946825d7f17d9011
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Sat Nov 25 14:26:37 2023 +0100
    
        psd faster for auto-spectrum, istft bug fix
    
        - _welch can now take the second argument as None and avoids unnecessary computations
        - istft can handle different fft lengths in case each frame is zero-padded
    
    commit 7763928b06a7dfd1af229f82a2112d840102362e
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Fri Nov 10 14:15:25 2023 +0100
    
        bug fix frequency-dependent windowing
    
    commit a6f2748417a1c96c3a3b071d3a87d66effa151d6
    Merge: e23e6c0 ec4a75e
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Fri Nov 3 16:50:41 2023 +0100
    
        Merge branch 'dev' of https://github.com/nico-franco-gomez/dsptoolbox into dev
    
    commit ec4a75ed5f4b5e6bae35a3a434ca82ff523026b8
    Merge: 874c2ba 8152cb9
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Fri Nov 3 16:49:35 2023 +0100
    
        Merge branch 'dev' of https://github.com/nico-franco-gomez/dsptoolbox into dev
    
    commit 874c2baa45dbac242764f286ffe63da48f700c54
    Author: Nicolas Franco Gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Fri Nov 3 16:47:54 2023 +0100
    
        v0.2.11 - Bug fixes (#35)
    
        * room acoustics integer types
    
        * ensure arrays are 1d
    
        * combine dirac, fractional smoothing
    
        * changelog, version
    
    commit 8152cb918ae62b11d5e86eaab492407d26bba312
    Author: Nicolas Franco Gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Fri Nov 3 16:47:54 2023 +0100
    
        v0.2.11 - Bug fixes (#35)
    
        * room acoustics integer types
    
        * ensure arrays are 1d
    
        * combine dirac, fractional smoothing
    
        * changelog, version
    
    commit 2b94fc0067c71a377488dcd0fac468baa38c88a8
    Merge: 073385d b94d170
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Wed Nov 1 14:38:41 2023 +0100
    
        Merge branch 'dev' of https://github.com/nico-franco-gomez/dsptoolbox into dev
    
    commit b94d170dc8642a3019dcfa63a3c3976ba81119e9
    Author: nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
    Date:   Wed Nov 1 14:36:04 2023 +0100
    
        bug fix

--- tests/test_transfer_functions.py
+++ tests/test_transfer_functions.py
@@ -395,22 +395,10 @@ class TestTransferFunctionsModule:
 
     def test_harmonics_from_chirp_ir(self):
         # Only functionality
-        ir = dsp.Signal(
-            "/Users/nico/Downloads/tests/some_new_ir.wav",
-            signal_type="rir",
-        )
-        harms = dsp.transfer_functions.harmonics_from_chirp_ir(
+        ir = dsp.Signal(join("examples", "data", "rir.wav"), signal_type="rir")
+        dsp.transfer_functions.harmonics_from_chirp_ir(
             ir,
             chirp_range_hz=[20, 20e3],
             chirp_length_seconds=2,
             n_harmonics=2,
         )
-
-        import matplotlib.pyplot as plt
-
-        fig, ax = plt.subplots(1, 1)
-        for ind, h in enumerate(harms):
-            freqs = np.fft.rfftfreq(len(h), 1 / ir.sampling_rate_hz)
-            s = np.fft.rfft(h)
-            ax.semilogx(freqs, 20 * np.log10(np.abs(s)))
-        plt.show()
