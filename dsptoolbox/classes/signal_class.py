scaling error corrected

dsptoolbox/classes/signal_class.py
@@ -1,6 +1,7 @@
 """
 Signal class
 """
+
 from warnings import warn
 from pickle import dump, HIGHEST_PROTOCOL
 from copy import deepcopy
@@ -794,7 +795,7 @@ class Signal:
                     self._spectrum_parameters["scaling"]
                     == "amplitude spectrum"
                 ):
-                    spectrum /= time_length
+                    spectrum *= 2 / time_length
             self.spectrum = []
             self.spectrum.append(
                 np.fft.rfftfreq(time_length, 1 / self.sampling_rate_hz)

tests/test_classes.py
@@ -1,6 +1,7 @@
 """
 Tests for basic functionalities of the classes in dsptoolbox
 """
+
 import pytest
 import dsptoolbox as dsp
 import numpy as np
@@ -77,7 +78,7 @@ class TestSignal:
             method="standard", scaling="amplitude spectrum"
         )
         _, sp_sig = s.get_spectrum()
-        assert np.all(sp / self.time_vec.shape[0] == sp_sig)
+        assert np.all(sp / self.time_vec.shape[0] * 2 == sp_sig)
 
         # Try smoothing
         s.set_spectrum_parameters(