corrected scaling

dsptoolbox/classes/signal_class.py
@@ -795,7 +795,12 @@ class Signal:
                     self._spectrum_parameters["scaling"]
                     == "amplitude spectrum"
                 ):
-                    spectrum *= 2 / time_length
+                    spectrum /= time_length
+                    # Scale one-sided spectrum except for DC and Nyquist
+                    if time_length % 2 == 0:
+                        spectrum[1:-1] *= 2
+                    else:
+                        spectrum[1:] *= 2
             self.spectrum = []
             self.spectrum.append(
                 np.fft.rfftfreq(time_length, 1 / self.sampling_rate_hz)

tests/test_classes.py
@@ -78,7 +78,12 @@ class TestSignal:
             method="standard", scaling="amplitude spectrum"
         )
         _, sp_sig = s.get_spectrum()
-        assert np.all(sp / self.time_vec.shape[0] * 2 == sp_sig)
+        sp /= self.time_vec.shape[0]
+        if self.time_vec.shape[0] % 2 == 0:
+            sp[1:-1] *= 2
+        else:
+            sp[1:] *= 2
+        assert np.all(sp == sp_sig)
 
         # Try smoothing
         s.set_spectrum_parameters(