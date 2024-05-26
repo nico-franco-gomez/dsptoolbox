f5151d0fc88ce3729a050e2073d04ad917c5169a
nico-franco-gomez <80042895+nico-franco-gomez@users.noreply.github.com>
2024-05-26 11:02:22 +0200

    added assertion in harmonic distortion analysis

--- dsptoolbox/transfer_functions/transfer_functions.py
+++ dsptoolbox/transfer_functions/transfer_functions.py
@@ -1641,14 +1641,17 @@ def harmonic_distortion_analysis(
             chirp_range_hz = [0, ir2.sampling_rate_hz // 2]
 
         passed_harmonics = True
-
     elif type(ir) is Signal:
+        assert (
+            chirp_length_s is not None
+            and chirp_range_hz is not None
+            and n_harmonics is not None
+        ), "Chirp parameters and number of harmonics cannot be None"
+
         # Get different harmonics
         harm = harmonics_from_chirp_ir(
             ir, chirp_range_hz, chirp_length_s, n_harmonics, 0.01
         )
-
-        passed_harmonics = False
 
         # Trim and window IR
         ir2 = ir.copy()
@@ -1658,6 +1661,8 @@ def harmonic_distortion_analysis(
         ir2.time_data = ir2.time_data[start:stop]
         ir2 = window_ir(ir2, len(ir2), constant_percentage=0.9)[0]
         ir2._spectrum_parameters["smoothe"] = smoothing
+
+        passed_harmonics = False
     else:
         raise TypeError("Type for ir is not supported")
 
