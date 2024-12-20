3cd28593f594714f8719a1a528e583126866931b
Nicolas Franco Gomez <80042895+nico-franco-gomez@users.noreply.github.com>
2024-12-20 11:33:46 +0100

    minor bug fix to abstract class

--- dsptoolbox/classes/realtime_filter.py
+++ dsptoolbox/classes/realtime_filter.py
@@ -3,17 +3,17 @@ import abc
 
 class RealtimeFilter(abc.ABC):
     @abc.abstractmethod
-    def process_sample(x: float, channel: int):
+    def process_sample(self, x: float, channel: int):
         """Process a sample with the filter for a given channel. Channel index
         is not checked for speed."""
         pass
 
     @abc.abstractmethod
-    def reset_state():
+    def reset_state(self):
         """Reset all filter states to 0."""
         pass
 
     @abc.abstractmethod
-    def set_n_channels(n_channels: int):
+    def set_n_channels(self, n_channels: int):
         """Set the number of channels to be filtered."""
         pass
