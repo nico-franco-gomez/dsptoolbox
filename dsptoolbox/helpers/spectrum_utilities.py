eb59f6221a252c937d594c581003ac2149e153f7
Nicolas Franco Gomez <80042895+nico-franco-gomez@users.noreply.github.com>
2025-01-27 15:37:47 +0100

    made correct phase spectrum capable of ndim == 2

--- dsptoolbox/helpers/spectrum_utilities.py
+++ dsptoolbox/helpers/spectrum_utilities.py
@@ -235,9 +235,23 @@ def _correct_for_real_phase_spectrum(phase_spectrum: NDArray[np.float64]):
 
     """
     factor = phase_spectrum[-1] % np.pi
-    return (
-        phase_spectrum
-        - np.linspace(0, 1, len(phase_spectrum), endpoint=True) * factor
+
+    # Single dimension
+    if phase_spectrum.ndim == 1:
+        return (
+            phase_spectrum
+            - np.linspace(0, 1, len(phase_spectrum), endpoint=True) * factor
+        )
+
+    # Two dims
+    assert phase_spectrum.ndim == 2, "More than 2 dimensions are not supported"
+    return phase_spectrum - (
+        np.repeat(
+            np.linspace(0, 1, len(phase_spectrum), endpoint=True)[..., None],
+            phase_spectrum.shape[1],
+            axis=1,
+        )
+        * factor[None, ...]
     )
 
 
